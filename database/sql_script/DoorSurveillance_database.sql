20c89fddc5af7e2bfaa858243d6f380bb77970bb
RafaelEscher <rafael-escher@hotmail.com>
2019-08-14 21:15:53 -0300

    script mysql das tabelas registros e cadastros

---
+++ database/sql_script/DoorSurveillance_database.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE acesso_db1
+	CHARACTER SET utf8
+	COLLATE utf8_unicode_ci;
+
+use acesso_db1;
+
+DROP TABLE cadastros;
+CREATE TABLE cadastros (
+	id INT(6) UNSIGNED AUTO_INCREMENT,
+    tagId VARCHAR(30) NOT NULL UNIQUE,
+    nome VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id, tagId)
+);
+
+DROP TABLE registros;
+CREATE TABLE registros (
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    tagId VARCHAR(30) NOT NULL,
+    nome VARCHAR(30) NOT NULL,
+    data TIMESTAMP,
+    estado TINYINT(1) DEFAULT NULL
+);
